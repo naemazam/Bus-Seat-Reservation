50e0a9f5085e02bbe5e6c99e6ef40d4abed32751
Naem Azam <78720392+naemazam@users.noreply.github.com>
2022-07-06 17:27:50 +0800

    Add files via upload

---
+++ index.html
@@ -0,0 +1,57 @@
+<!doctype html>
+
+
+<html>
+
+<head>
+    <title>Simple Bus Seat Reservation Site</title>
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/jquery.seat-charts.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="js/jquery.seat-charts.js"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="grid-50">
+                    <div id="seat-map">
+                        <div class="front-indicator">Bus Seat Reservation</div>
+                        <h4 class="text-muted fw-bold text-center" style="padding-left:3em; margin:.5em">From Front Row</h4>
+                        <div id="bus-seat-map"></div>
+                        <h4 class="text-muted fw-bold text-center" style="padding-left:3em; margin:.5em">End of Seat Row</h4>
+                    </div>
+                </div>
+                <div class="grid-50">
+                    <div class="booking-details">
+
+                        <form action="" method="post">
+
+                            <h2>Booking Details</h2>
+
+                            <h3> Selected Seats (<span id="counter">0</span>):</h3>
+                            <ul id="selected-seats"></ul>
+
+                            <h2>Total: <b>$<span id="total">0</span></b></h2>
+
+                            <button type="button" id="checkout-button">Submit Book</button>
+
+                        </form>
+
+                        <div id="legend"></div>
+                        <button id="reset-btn" type="button">Reset Bus Seat</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,142 @@
+var firstSeatLabel = 1;
+var booked = !!localStorage.getItem('booked') ? $.parseJSON(localStorage.getItem('booked')) : [];
+$(document).ready(function() {
+    var $cart = $('#selected-seats'),
+        $counter = $('#counter'),
+        $total = $('#total'),
+        sc = $('#bus-seat-map').seatCharts({
+            map: [
+                'ff_ff',
+                'ff_ff',
+                'ee_ee',
+                'ee_ee',
+                'ee___',
+                'ee_ee',
+                'ee_ee',
+                'ee_ee',
+                'eeeee',
+            ],
+            seats: {
+                f: {
+                    price: 75,
+                    classes: 'first-class', //your custom CSS class
+                    category: 'First Class'
+                },
+                e: {
+                    price: 50,
+                    classes: 'economy-class', //your custom CSS class
+                    category: 'Economy Class'
+                }
+
+            },
+            naming: {
+                top: false,
+                getLabel: function(character, row, column) {
+                    return firstSeatLabel++;
+                },
+            },
+            legend: {
+                node: $('#legend'),
+                items: [
+                    ['f', 'available', 'First Class'],
+                    ['e', 'available', 'Economy Class'],
+                    ['f', 'unavailable', 'Already Booked']
+                ]
+            },
+            click: function() {
+                if (this.status() == 'available') {
+                    //let's create a new <li> which we'll add to the cart items
+                    $('<li>' + this.data().category + ' Seat # ' + this.settings.label + ': <b>$' + this.data().price + '</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
+                        .attr('id', 'cart-item-' + this.settings.id)
+                        .data('seatId', this.settings.id)
+                        .appendTo($cart);
+
+                    /*
+                     * Lets update the counter and total
+                     *
+                     * .find function will not find the current seat, because it will change its stauts only after return
+                     * 'selected'. This is why we have to add 1 to the length and the current seat price to the total.
+                     */
+                    $counter.text(sc.find('selected').length + 1);
+                    $total.text(recalculateTotal(sc) + this.data().price);
+
+                    return 'selected';
+
+                } else if (this.status() == 'selected') {
+
+                    //update the counter
+                    $counter.text(sc.find('selected').length - 1);
+
+                    //and total
+                    $total.text(recalculateTotal(sc) - this.data().price);
+
+                    //remove the item from our cart
+                    $('#cart-item-' + this.settings.id).remove();
+
+                    //seat has been vacated
+                    return 'available';
+
+                } else if (this.status() == 'unavailable') {
+                    //seat has been already booked
+                    return 'unavailable';
+                } else {
+                    return this.style();
+                }
+            }
+        });
+
+    //this will handle "[cancel]" link clicks
+    $('#selected-seats').on('click', '.cancel-cart-item', function() {
+        //let's just trigger Click event on the appropriate seat, so we don't have to repeat the logic here
+        sc.get($(this).parents('li:first').data('seatId')).click();
+    });
+
+    //let's pretend some seats have already been booked
+    // sc.get(['1_2', '4_1', '7_1', '7_2']).status('unavailable');
+    sc.get(booked).status('unavailable');
+
+});
+
+function recalculateTotal(sc) {
+    var total = 0;
+
+    //basically find every selected seat and sum its price
+    sc.find('selected').each(function() {
+
+        total += this.data().price;
+
+    });
+
+    return total;
+}
+
+$(function() {
+    $('#checkout-button').click(function() {
+        var items = $('#selected-seats li')
+        if (items.length <= 0) {
+            alert("Please select atleast 1 seat first.")
+            return false;
+        }
+        var selected = [];
+        items.each(function(e) {
+            var id = $(this).attr('id')
+            id = id.replace("cart-item-", "")
+            selected.push(id)
+        })
+        if (Object.keys(booked).length > 0) {
+            Object.keys(booked).map(k => {
+                selected.push(booked[k])
+            })
+        }
+        localStorage.setItem('booked', JSON.stringify(selected))
+        alert("Seats has been Reserved successfully.")
+        location.reload()
+    })
+    $('#reset-btn').click(function() {
+        if (confirm("are you sure to reset the reservation of the bus?") === true) {
+            localStorage.removeItem('booked')
+            alert("Seats has been Reset successfully.")
+            location.reload()
+        }
+    })
+})
